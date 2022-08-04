cdc863464b5fdc139bce077859d548b937cd353e
LuzBenita <luz@gserrano.mx>
2022-08-04 12:36:20 -0500

    Cambio de nombre a la carpeta flota

--- flota/models/flota_serrano.py
+++ flota/models/flota_serrano.py
@@ -1,4 +1,5 @@
 from odoo import fields, models,api
+
 class FlotaSerrano(models.Model):
 
     _inherit = "sale.order"
@@ -29,3 +30,62 @@ class FlotaSerrano(models.Model):
         ondelete='set null',
         index=True,
     )
+#    _sql_constraints = [
+#        ('name_description_check',
+#         'CHECK(name != description)',
+#         "The title of the course should not be the description"),
+
+#        ('name_unique',
+#         'UNIQUE(name)',
+#         "The course title must be unique"),
+#    ]
+    
+#    no_economico = fields.Char(
+#        string="No.Económico",
+#        required=True,
+#    )
+#    placa = fields.Char(
+#        string="Placas",
+#        required=True,
+#    )
+    
+
+    
+    #def fun_rend(self):
+     #   return '2.2'
+
+   
+    #def _onchange_disel(self):
+    # set auto-changing field
+    #    self.disel = self.km / self.rendimiento
+        # Can optionally return a warning and domains
+    #    return {
+    #        'warning': {
+    #            'title': "Something bad happened",
+    #            'message': "It was very bad indeed",
+    #        }
+    #    }
+#    no_chasis = fields.Char(
+#        string="Chasis",
+#        required=True,
+#    )
+#    f_registro = fields.Char(
+#        string="Fecha de Registro",
+#        required=True,
+#    )
+#    empresa = fields.Char(
+#        string="Empresa",
+#        required=True,
+#    )
+  
+#    def copy(self, default=None):
+#        default = dict(default or {})
+#        copied_count = self.search_count(
+#            [('name', '=like', u"Copy of {}%".format(self.name))])
+#        if not copied_count:
+#            new_name = u"Copy of {}".format(self.name)
+#        else:
+#            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+#
+#        default['name'] = new_name
+#        return super(FlotaSerrano, self).copy(default)
