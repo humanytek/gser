cf092662f514b61105c078a45396c2a002c9d828
LuzBenita <luz@gserrano.mx>
2022-08-05 12:33:25 -0500

    campos a flota 1.5

--- flota/models/flota_serrano.py
+++ flota/models/flota_serrano.py
@@ -3,14 +3,14 @@ from odoo import fields, models, api
 class FlotaSerrano(models.Model):
 
     _inherit = "fleet.vehicle"
-
+                    
     tipo_vehiculo = fields.Char(
         string="Marca",
     )
     no_economico = fields.Char(
         string="Modelo",
     )
-    empresa = fields.Many2one(
+    empresa = fields.Char(
         string="Empresa",
         #comodel_name='res.company',
        # ondelete='set null',
