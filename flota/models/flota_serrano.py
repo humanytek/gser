4ce9c09521f7cc5d31b6de342016e52dfd27e70f
LuzBenita <luz@gserrano.mx>
2022-08-05 12:44:26 -0500

    campos a flota 1.7

--- flota/models/flota_serrano.py
+++ flota/models/flota_serrano.py
@@ -5,14 +5,13 @@ class FlotaSerrano(models.Model):
     _inherit = "fleet.vehicle"
                     
     tipo_vehiculo = fields.Char(
-        string="Marca",
+        string="Tipo de vehiculo",
     )
     no_economico = fields.Char(
-        string="Modelo",
+        string="No. economico",
     )
-    empresa = fields.Char(
-        string="Empresa",
-        #comodel_name='res.company',
-       # ondelete='set null',
-       # index=True,
+    empresa = fields.Many2one(
+        comodel_name='res.company',
+        ondelete='set null',
+        index=True,
     )
