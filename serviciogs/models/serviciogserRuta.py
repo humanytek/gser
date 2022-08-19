90493d644fde6a57d380fa40653bd33e14e84105
LuzBenita <luz@gserrano.mx>
2022-08-19 14:33:14 -0500

    relacion email 4.13

--- serviciogs/models/serviciogserRuta.py
+++ serviciogs/models/serviciogserRuta.py
@@ -4,15 +4,6 @@ class servicioruta(models.Model):
     _name = 'servicio.ruta'
     _description = 'Rutas'
 
-    _sql_constraints = [
-        ('name_description_check',
-         'CHECK(name != description)',
-         "The title of the course should not be the description"),
-
-        ('name_unique',
-         'UNIQUE(name)',
-         "The course title must be unique"),
-    ]
     contacto_facturacion = fields.Char(
         string="Conctacto Facturacion",
         required=True,
@@ -25,14 +16,3 @@ class servicioruta(models.Model):
         string ="Orden de venta",
     )
    
-    def copy(self, default=None):
-        default = dict(default or {})
-        copied_count = self.search_count(
-            [('name', '=like', u"Copy of {}%".format(self.name))])
-        if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
-        else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
-
-        default['name'] = new_name
-        return super(servicioGSer, self).copy(default)
