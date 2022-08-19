87e0aec2c6cfa9994ef9441d081259833e1213d9
LuzBenita <luz@gserrano.mx>
2022-08-19 12:09:48 -0500

    relacion email 3.18

--- serviciogs/models/serviciogser.py
+++ serviciogs/models/serviciogser.py
@@ -18,7 +18,7 @@ class servicioGSer (models.Model):
         string="Contacto Facturación",
     )
     email_facturacion = fields.Char(
-        related ='partner_id.child_id.email',
+        related ='partner_id.child_ids.email',
         string="E-mail Facturación",
     )
     km_ruta = fields.Float(
