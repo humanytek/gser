84dce4b2454f2692f3cdb1a2c55a1b0a2d0fe3dc
LuzBenita <luz@gserrano.mx>
2022-08-19 15:42:21 -0500

    modulo de ruta 1.5

--- serviciogs/models/serviciogserRuta.py
+++ serviciogs/models/serviciogserRuta.py
@@ -1,16 +1,16 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class ServicioRuta(models.Model):
-    _name = 'servicio.ruta'
+    _name = 'serviciogserRuta'
     _description = 'Rutas'
 
     contacto_facturacion = fields.Char(
         string="Conctacto Facturacion",
-        required=True,
+        
     )
     email_facturacion = fields.Char(
         string="email",
-        required=True,
+     
     )
     ord_vent = fields.Char(
         string ="Orden de venta",
