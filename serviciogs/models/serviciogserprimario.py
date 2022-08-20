277cd1987ddc629b1da4ba950e61e3bde8fb9868
LuzBenita <luz@gserrano.mx>
2022-08-20 12:14:00 -0500

    modulo de ruta 1.17

--- serviciogs/models/serviciogserprimario.py
+++ serviciogs/models/serviciogserprimario.py
@@ -25,7 +25,7 @@ class servicioGSerprimario (models.Model):
         string="Vehiculo",
     )
     anio_vehiculo = fields.Char(
-        related ='vehicle.model.model_year',
+        related ='model.model_year',
         string="Año vehículo",
     )
 
