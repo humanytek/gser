b0915d9e2ef3a30172502c80a37cd3fa29e528b9
LuzBenita <luz@gserrano.mx>
2022-08-27 10:28:25 -0500

    cambios en vista principal de servicio ext 1.28

--- serviciogs/models/serviciogserprimario.py
+++ serviciogs/models/serviciogserprimario.py
@@ -58,7 +58,7 @@ class servicioGSerprimario (models.Model):
         string ="Tipo Ruta",
     )
     carga_viaje= fields.Char(
-        related ='project_id.partner_id.name',
+        related ='project_id.partner_id.function',
         string ="Carga",
     )  
     capacidad_viaje = fields.Char(
