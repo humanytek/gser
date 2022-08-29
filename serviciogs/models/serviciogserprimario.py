3adb8d447ca81375e893dd8ba19ca8e647b404ea
LuzBenita <luz@gserrano.mx>
2022-08-29 12:20:16 -0500

    Campos de Facturacion cobranza 1.19

--- serviciogs/models/serviciogserprimario.py
+++ serviciogs/models/serviciogserprimario.py
@@ -115,7 +115,7 @@ class servicioGSerprimario (models.Model):
         string ="Retención 4%",
     )   
     Total_Facturar = fields.Float(
-        compute ='_compute_totalfacturar',
+       # compute ='_compute_totalfacturar',
         string ="Total a Facturar",
     )
     Forma_pago = fields.Selection([
@@ -148,8 +148,4 @@ class servicioGSerprimario (models.Model):
         for record in self:
             record.Retencion = record.Subtotal * 0.04
 
-    @api.depends("Subtotal", "Iva", "Retencion")
-    def _compute_totalfacturar(self):
-        for record in self:
-            record.Total_Facturar = (record.Subtotal + record.Iva) - record.Retencion
-    
+   
