a874f4249d08da5d420db80708268a89d4841396
LuzBenita <luz@gserrano.mx>
2022-09-07 16:22:53 -0500

    report ticket cliente y lavado 1.29

--- serviciogs/__manifest__.py
+++ serviciogs/__manifest__.py
@@ -16,7 +16,7 @@
         # demo
         # reports
         'report/reports.xml',
-        #'report/ticket_viaje.xml',
+        'report/ticket_viaje.xml',
         'report/ticket_cliente.xml',
         #'report/proceso_lavado.xml',
         # # views
