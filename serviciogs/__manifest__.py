22e7913685197c536edf0e54d4a9ade5cb49ce9c
LuzBenita <luz@gserrano.mx>
2022-11-07 12:35:17 -0600

    subida reṕortes

--- serviciogs/__manifest__.py
+++ serviciogs/__manifest__.py
@@ -19,11 +19,11 @@
         'views/serviciogser.xml',
         'views/serviciogserprimario.xml'
         # reports
-        #'report/reports.xml',
-        #'report/ticket_viaje.xml',
-        #'report/ticket_cliente.xml',
-        #'report/proceso_lavado.xml',
-        #'report/cp_vertical.xml',
-        #'report/inspeccion_trans.xml',
+        'report/reports.xml',
+        'report/ticket_viaje.xml',
+        'report/ticket_cliente.xml',
+        'report/proceso_lavado.xml',
+        'report/cp_vertical.xml',
+        'report/inspeccion_trans.xml',
     ],
 }
