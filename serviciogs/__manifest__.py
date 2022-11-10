2dc3dc7e628bb7d60e0f0f92e94bbfd30be01b5a
LuzBenita <luz@gserrano.mx>
2022-11-10 12:47:35 -0600

    cambio

--- serviciogs/__manifest__.py
+++ serviciogs/__manifest__.py
@@ -20,10 +20,10 @@
         'views/serviciogserprimario.xml',
         # reports
         #'report/reports.xml',
-        'report/ticket_viaje.xml',
-        'report/ticket_cliente.xml',
-        'report/proceso_lavado.xml',
-        'report/cp_vertical.xml',
-        'report/inspeccion_trans.xml',
+        #'report/ticket_viaje.xml',
+        #'report/ticket_cliente.xml',
+        #'report/proceso_lavado.xml',
+        #'report/cp_vertical.xml',
+        #'report/inspeccion_trans.xml',
     ],
 }
