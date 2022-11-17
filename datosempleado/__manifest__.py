423b90d9a17f4799aa0f196698aad3a0afedcde5
alanrgs <alan.ruiz@gserrano.mx>
2022-11-17 11:40:16 -0600

    up datos empleado

---
+++ datosempleado/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Datos Empleado',
+    'version': '13.0.0.4.0',
+    'author': 'GSerrano Lagos de Moreno',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [
+        "hr"
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        #'views/datosempleados.xml',
+    ],
+}
