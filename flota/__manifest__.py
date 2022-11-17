0edefd3cec3b0be1e884ff0d9d18da135f2c1235
alanrgs <alan.ruiz@gserrano.mx>
2022-11-17 12:46:49 -0600

    up flota fix

---
+++ flota/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Flota Serrano',
+    'version': '13.0.0.4.0',
+    'author': 'GS Lagos de Moreno',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [
+        "fleet",
+    ],
+    'data': [
+        # views
+        'views/flota_serrano.xml',
+    ],
+}
