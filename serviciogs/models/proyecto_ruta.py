4d56903c4186193113e7749001eed4d7883aeeb1
alanrgs <alan.ruiz@gserrano.mx>
2022-11-18 20:13:43 -0600

    cambio rendimiento caja

--- serviciogs/models/proyecto_ruta.py
+++ serviciogs/models/proyecto_ruta.py
@@ -35,14 +35,14 @@ class proyecto_ruta (models.Model):
         string="Kilometros",
     )
     carga_ruta = fields.Selection([
-        ('Full'),
-        ('Sencillo'),
-        ('Caja seca'),
-        ('Gondola'),
-        ('Plataforma'),
-        ('Torton'),
-        ('N/A'),
-        ('12'),],
+        ('1','Full'),
+        ('2','Sencillo'),
+        ('3','Caja seca'),
+        ('4','Gondola'),
+        ('5','Plataforma'),
+        ('6','Torton'),
+        ('7','N/A'),
+        ('8','12'),],
         string="Carga",
     )
     capacidad_ruta = fields.Selection([
@@ -186,8 +186,10 @@ class proyecto_ruta (models.Model):
     @api.depends("carga_ruta","rendimiento_diesel")
     def _calculo_rendimeinto(self):
         for record in self:
-            if record.carga_ruta == "Full":
-                record.rendimiento_diesel = 5
+            if record.carga_ruta == '1':
+                record.rendimiento_diesel = 1.6
+            if record.carga_ruta == '3':
+                record.rendimiento_diesel = 2.3
             else:
-                record.rendimiento_diesel = 2.1
+                record.rendimiento_diesel = 2.2
             
