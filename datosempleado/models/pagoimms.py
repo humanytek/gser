a7a2339d047944df10e5769830713da9cdd6f07f
alanrgs <alan.ruiz@gserrano.mx>
2023-01-04 17:00:05 -0600

    add pago seguro

---
+++ datosempleado/models/pagoimms.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+class datosEmpleados (models.Model):
+    _inherit="hr.employee"
+    #Modulo para subir los pagos de imms
+    pago_imms = fields.Binary(
+        string="Pago de Seguro",
+    )
+    fecha_pago = fields.Date(    
+        string="Fecha pago",
+    )
