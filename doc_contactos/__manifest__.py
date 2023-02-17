e4a8e70ad8790fd229187058027ff57ee62629cf
alanrgs <alan.ruiz@gserrano.mx>
2023-02-17 16:56:59 -0600

    create doc_contactos

---
+++ doc_contactos/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Docuementacion Contactos',
+    'version': '13.0.0.4.0',
+    'author': 'GSerrano Lagos de Moreno',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [
+        "contacts",
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        
+        # # views
+        'views/documentos_contactos.xml',
+    ],
+}
