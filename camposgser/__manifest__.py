617e6e4b8f018a166df988e8fb770cf33650cbe2
ferguez1989 <frodriguez@beeren.com.mx>
2023-03-01 21:29:34 -0600

    Campos

---
+++ camposgser/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Campos Serrano',
+    'version': '15.0.0.1.0',
+    'author': 'Fernando Rodriguez',
+    'summary': 'Ajuste de Nombre de Campos',
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        # views
+        'views/camposajuste.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ camposgser/models/campos_ajuste.py
@@ -0,0 +1,5 @@
+from odoo import fields, models
+
+
+class campos_ajuste (models.Model):
+    _inherit = "hr.employee"
