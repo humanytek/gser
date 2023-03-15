c124695a6c55a5bdb75b10fce3a42cfd2a00bb84
ferguez1989 <f.rodriguez@beeren.com.mx>
2023-03-14 20:57:11 -0600

    Docuementacion Contactos Fernando 2023

--- doc_contactos/models/documentos_contactos.py
+++ doc_contactos/models/documentos_contactos.py
@@ -24,4 +24,11 @@ class datosEmpleados (models.Model):
     descripcion = fields.Binary(
         string="Descripcion",
     )
-    
+    #Fernando 2023
+    f_licencia = fields.Date(
+        string="Vigencia de licencia",
+    )
+
+    doc_f_licencia = fields.Binary(
+        string="Licencia",
+    )
