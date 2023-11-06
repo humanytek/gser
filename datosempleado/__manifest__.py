edc01c28228ecb22239fec4dad49cf2b8b21b0bc
Ricardo Aucapina <raucapinab96@gmail.com>
2023-11-06 12:07:31 -0500

    local_to_gsertest

--- datosempleado/__manifest__.py
+++ datosempleado/__manifest__.py
@@ -1,7 +1,7 @@
 {
-    'name': 'Datos Empleado TEST LOCAL',
+    'name': 'Datos Empleado',
     'version': '15.0.0.4.0',
-    'author': 'GSerrano Lagos de Moreno',
+    'author': 'GSerrano Lagos de Moreno TEST ',
     'website': '',
     'license': 'LGPL-3',
     'depends': [
