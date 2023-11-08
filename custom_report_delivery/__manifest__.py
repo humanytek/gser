d3a0e46ccdd2a2806fe878956ae245b9653a42ba
RicardoAucapina <69064872+RicardoAucapina@users.noreply.github.com>
2023-11-08 10:18:12 -0500

    Agregar personalizacion de reporte de entrega

---
+++ custom_report_delivery/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Personalizacion de reportes',
+    'version': '15.0.0.4.0',
+    'author': 'Ricardo Aucapina GS',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [
+        "stock"
+    ],
+    'data': [
+        'views/custom_report_delivery.xml',
+    ],
+}
