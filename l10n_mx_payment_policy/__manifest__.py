4f3824837168c5f5d7c4018e47e6b0c6b8fc3c42
RicardoAucapina <69064872+RicardoAucapina@users.noreply.github.com>
2024-03-01 14:19:38 -0600

    Validar Terminos de pago

---
+++ l10n_mx_payment_policy/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "L10N Politica de pago",
+    "version": "13.0.0.1.0",
+    "author": "Ricardo Aucapiña",
+    "license": "LGPL-3",
+    "depends": [
+        "l10n_mx_edi",
+    ],
+    "data": [
+    ],
+}
