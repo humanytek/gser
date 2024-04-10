72c15df44de72a4f0865adc99b1091728bd48fab
RicardoAucapina <69064872+RicardoAucapina@users.noreply.github.com>
2024-04-10 14:03:35 -0600

    Conector sh-aws

---
+++ db_con_aws/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Conector-AWS-GSER',
+    'version': '15.0.0.0.0',
+    'author': 'Ricardo Aucapina, gser',
+    'license': 'LGPL-3',
+    'depends': [
+        "base",
+    ],
+    'data': [
+        "security/ir.model.access.csv",
+        # data
+        # demo
+        
+        #views
+    ],
+}
